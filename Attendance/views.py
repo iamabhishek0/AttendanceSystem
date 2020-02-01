3dd1f62ca78b1b8e7a1b6874f72b96b7af450c54
prshnt19 <prashant.rawat216@gmail.com>
2020-02-01 10:50:10 +0530

    video data

--- Attendance/views.py
+++ Attendance/views.py
@@ -3,7 +3,7 @@ from voiceit2 import VoiceIt2
 
 # Create your views here.
 # usr_6a69dbdcedca4d6ea82c90a9af31b9f5 prashant
-# voiceitsiddharth
+# usr_afca986a9db2473d932228735d298957 siddharth
 # usr_361ea5aa209047189cc7222ff249a4e5 arpit
 # grp_b3f953e210494f5db673bbd520ff79d8
 
@@ -20,7 +20,7 @@ def enroll_user(request):
     api_key = 'key_66f6eb3dbd0c4d7d85bf9e716b3813f4'
     api_token = 'tok_eff69e97da604bf7a6d13b8ed1400ce9'
     my_voiceit = VoiceIt2(api_key, api_token)
-    user_id = 'afca986a9db2473d932228735d298957'
+    user_id = 'usr_afca986a9db2473d932228735d298957'
     content_language = 'en-US'
     phrase = 'my face and voice identify me'
     video = open('siddharth1.mp4', 'rb')
